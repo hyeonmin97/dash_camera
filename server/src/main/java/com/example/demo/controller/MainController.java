f4e8994898451581c792a2dc094ee86132f9fc7e
hyeonmin <hyeonmin97@naver.com>
2021-08-13 22:23:12 +0900

    modify MainController
    리턴값 변경

--- server/src/main/java/com/example/demo/controller/MainController.java
+++ server/src/main/java/com/example/demo/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+
 import org.springframework.core.io.UrlResource;
 import org.springframework.core.io.support.ResourceRegion;
 import org.springframework.http.*;
@@ -7,9 +8,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -18,44 +22,44 @@ public class MainController {
 
     //1. 폴더든 파일이든 리스트만 보이게
     @GetMapping("/test1")
-    public String test(){
-        File dir = new File("C:\\Users\\hyeonmin\\Desktop\\video");
+    public @ResponseBody File[] test(){
+        File dir = new File("C:\\Users\\hyeonmin\\Desktop\\dash_camera\\webcam\\videos");
         File files[] = dir.listFiles();
         System.out.println("in Maincontroller");
         for (int i = 0; i < files.length; i++) {
             System.out.println("file: " + files[i]);
         }
-        return "/test.html";
+        return files;
     }
 
-
+    @GetMapping("/test2")
     //2. 폴더는 폴더안에 들어가서 파일목록 출력(재귀)
-    public void list() {
-        System.out.println("2");
-        showFilesInDIr("/home/mjs/test/test");
-
+    public @ResponseBody List<String> list() {
+        List<String> fileList = new ArrayList<String>();
+        showFilesInDIr("C:\\Users\\hyeonmin\\Desktop\\dash_camera\\webcam\\videos", fileList);
+        return fileList;
     }
-    public static void showFilesInDIr(String dirPath) {
+    public static void showFilesInDIr(String dirPath, List<String> fileList) {
         File dir = new File(dirPath);
         File files[] = dir.listFiles();
-
         for (int i = 0; i < files.length; i++) {
             File file = files[i];
             if (file.isDirectory()) {
-                showFilesInDIr(file.getPath());
+                showFilesInDIr(file.getPath(),fileList);
+
             } else {
                 System.out.println("file: " + file);
+                fileList.add(file.getAbsolutePath());
             }
         }
     }
 
 
     //스트리밍
-    @GetMapping(value = "/video/{name}")
-    public ResponseEntity<ResourceRegion> getVideo(@RequestHeader HttpHeaders headers, @PathVariable String name) throws IOException {
+    @GetMapping(value = "/video1/{directory}/{name}")
+    public ResponseEntity<ResourceRegion> getVideo(@RequestHeader HttpHeaders headers, @PathVariable String directory, @PathVariable String name) throws IOException {
 
-
-        UrlResource video = new UrlResource("file:C:\\Users\\hyeonmin\\Desktop\\video\\"+name+".mp4");
+        UrlResource video = new UrlResource("file:C:\\Users\\hyeonmin\\Desktop\\dash_camera\\webcam\\videos\\"  + directory+ "\\" + name + ".mp4");
         ResourceRegion resourceRegion;
 
         final long chunkSize = 1000000L;
